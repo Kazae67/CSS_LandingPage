244527abd0b1ca8d86e78fe41620a02cadf5519d
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-28 23:34:06 +0200

    ajout de la navbar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Landing Page PDO</title>
+</head>
+<body>
+    <!-- Accueil-->
+
+    <!-- Navbar -->
+    <nav>
+        <div class="navbar-logo">
+            <p>Kucra</p>
+        </div>
+        <div class="navbar-links">
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">FEATURES</a></li>
+                <li><a href="#">CLIENT</a></li>
+                <li><a href="#pricing-section">PRICING</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">BLOG</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>

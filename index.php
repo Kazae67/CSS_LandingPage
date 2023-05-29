c364cfcc5361515178db0fc23f0c98386f984b8c
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-29 18:13:53 +0200

    mise en place des social-icons

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <script src="https://kit.fontawesome.com/a45e9c27c8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Landing Page PDO</title>
 </head>
 <body>
   <header>
-  <div class="logo-container">
-    <h1>Kucra</h1> <!-- Logo "Kucra" -->
-  </div>
+    <div class="logo-container">
+      <h1>Kucra</h1> <!-- Logo "Kucra" -->
+    </div>
     <nav class="navbar"> 
       <ul class="nav-links">
         <li><a href="#home">HOME</a></li>
@@ -23,6 +23,11 @@
         <li><a href="#blog">BLOG</a></li>
         <li><a href="#contact">CONTACT</a></li>
       </ul>
+      <ul class="social-icons">
+        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+      </ul>
     </nav>
   </header>
 
@@ -32,10 +37,10 @@
         <h1>We are StartUp Creative Kucra Agency</h1>
         <p>Carefully crafted after analyzing the needs of different industries, and the design achieves a great balance between purpose & presentation.</p>
         <form class="email-form" action="" method="post">
-        <div class="email-container">
+          <div class="email-container">
             <input type="email" name="email" placeholder="Enter your email" required>
             <button type="submit">SUBSCRIBE</button>
-        </div>
+          </div>
         </form>
       </div>
       <div class="right-container">

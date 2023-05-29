3ec5faccef7812dd20342820b5fc296b6f54650b
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-29 22:44:07 +0200

    Organisation des tabulations et ajout de commentaires

--- index.php
+++ index.php
@@ -8,48 +8,55 @@
     <title>Landing Page PDO</title>
 </head>
 <body>
-  <header>
-    <div class="logo-container">
-      <h1>Kucra</h1> <!-- Logo "Kucra" -->
-    </div>
-    <nav class="navbar"> 
-      <ul class="nav-links">
-        <li><a href="#home">HOME</a></li>
-        <li><a href="#features">FEATURES</a></li>
-        <li><a href="#client">CLIENT</a></li>
-        <li><a href="#pricing">PRICING</a></li>
-        <li><a href="#faq">FAQ</a></li>
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#blog">BLOG</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-      </ul>
-<ul class="social-icons social-container">
-    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
-    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-</ul>
-    </nav>
-  </header>
+    <!-- En-tête -->
+    <header>
+        <!-- Conteneur du logo -->
+        <div class="logo-container">
+            <h1>Kucra</h1>
+        </div>
+        <!-- Navbar -->
+        <nav class="navbar"> 
+            <ul class="nav-links">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#features">FEATURES</a></li>
+                <li><a href="#client">CLIENT</a></li>
+                <li><a href="#pricing">PRICING</a></li>
+                <li><a href="#faq">FAQ</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#blog">BLOG</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+            </ul>
+            <!-- Icônes sociales -->
+            <ul class="social-icons social-container">
+                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+        </nav>
+    </header>
 
-  <section id="home" class="section">
-    <div class="container">
-      <div class="left-container">
-        <h1>We are StartUp Creative Kucra Agency</h1>
-        <p>Carefully crafted after analyzing the needs of different industries, and the design achieves a great balance between purpose & presentation.</p>
-        <form class="email-form" action="" method="post">
-          <div class="email-container">
-            <input type="email" name="email" placeholder="Enter your email" required>
-            <button type="submit">SUBSCRIBE</button>
-          </div>
-        </form>
-      </div>
-      <div class="right-container">
-        <img src="img/illustration.svg" alt="Chat Image">
-      </div>
-    </div>
-  </section>
+    <!-- Section HOME -->
+    <section id="home" class="section">
+        <div class="container">
+            <!-- Conteneur de gauche -->
+            <div class="left-container">
+                <h1>We are StartUp Creative Kucra Agency</h1>
+                <p>Carefully crafted after analyzing the needs of different industries, and the design achieves a great balance between purpose & presentation.</p>
+                <form class="email-form" action="" method="post">
+                    <div class="email-container">
+                        <input type="email" name="email" placeholder="Enter your email" required>
+                        <button type="submit">SUBSCRIBE</button>
+                    </div>
+                </form>
+            </div>
+            <!-- Conteneur de droite -->
+            <div class="right-container">
+                <img src="img/illustration.svg" alt="Chat Image">
+            </div>
+        </div>
+    </section>
 
-  <!-- Les autres sections à venir -->
+    <!-- Les autres sections à venir -->
 
 </body>
 </html>
